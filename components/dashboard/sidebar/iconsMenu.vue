2cfd520cffadd2b137dbacfc9b6dbffed4c507b9
kapalexis <kap.alexis@gmail.com>
2022-04-27 11:35:42 +0200

    feature: icons from sidebars menu are reusable components

--- components/dashboard/sidebar/iconsMenu.vue
+++ components/dashboard/sidebar/iconsMenu.vue
@@ -3,9 +3,10 @@
     <div
       class="flex flex-col justify-start items-center py-4 gap-4"
     >
-      <NuxtLink to="/" class="h-14 w-14 border-l-2 border-transparent flex flex-col justify-start items-center py-4 gap-4 text-gray-300 hover:text-green-400 hover:border-l-2 hover:border-green-400 transition-all">
+      <dashboardSidebarIconsMenuItems linked-to="/">
         <svg
           xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -13,8 +14,8 @@
         >
           <path stroke-linecap="round" stroke-linejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
         </svg>
-      </NuxtLink>
-      <span class="h-14 w-14 border-l-2 border-transparent flex flex-col justify-start items-center py-4 gap-4 text-gray-300 hover:text-green-400 hover:border-l-2 hover:border-green-400 transition-all">
+      </dashboardSidebarIconsMenuItems>
+      <dashboardSidebarIconsMenuItems linked-to="#">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -24,9 +25,10 @@
         >
           <path stroke-linecap="round" stroke-linejoin="round" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
         </svg>
-      </span>
+      </dashboardSidebarIconsMenuItems>
+
       <hr class="h-1 w-full text-black border-[#ffffff20]">
-      <span class="h-14 w-14 border-l-2 border-transparent flex flex-col justify-start items-center py-4 gap-4 text-gray-300 hover:text-green-400 hover:border-l-2 hover:border-green-400 transition-all">
+      <dashboardSidebarIconsMenuItems linked-to="#">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -36,8 +38,9 @@
         >
           <path stroke-linecap="round" stroke-linejoin="round" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />
         </svg>
-      </span>
-      <span class="h-14 w-14 border-l-2 border-transparent flex flex-col justify-start items-center py-4 gap-4 text-gray-300 hover:text-green-400 hover:border-l-2 hover:border-green-400 transition-all">
+      </dashboardSidebarIconsMenuItems>
+
+      <dashboardSidebarIconsMenuItems linked-to="#">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -47,14 +50,10 @@
         >
           <path stroke-linecap="round" stroke-linejoin="round" d="M7 4v16M17 4v16M3 8h4m10 0h4M3 12h18M3 16h4m10 0h4M4 20h16a1 1 0 001-1V5a1 1 0 00-1-1H4a1 1 0 00-1 1v14a1 1 0 001 1z" />
         </svg>
-      </span>
+      </dashboardSidebarIconsMenuItems>
     </div>
 
-    <span
-      class="
-    h-14 w-14 border-l-2 border-transparent flex flex-col justify-start items-center py-4 gap-4 text-gray-300 hover:text-green-400 hover:border-l-2 hover:border-green-400 mb-2 transition-all
-    "
-    >
+    <dashboardSidebarIconsMenuItems linked-to="#">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -64,7 +63,7 @@
       >
         <path stroke-linecap="round" stroke-linejoin="round" d="M13 5l7 7-7 7M5 5l7 7-7 7" />
       </svg>
-    </span>
+    </dashboardSidebarIconsMenuItems>
   </div>
 </template>
 
---
+++ components/dashboard/sidebar/iconsMenuItems.vue
@@ -0,0 +1,18 @@
+<template>
+  <NuxtLink :to="linkedTo" class="h-14 w-14 border-l-2 border-transparent flex flex-col justify-start items-center py-4 gap-4 text-gray-300 hover:text-green-400 hover:border-l-2 hover:border-green-400 transition-all">
+    <slot />
+  </NuxtLink>
+</template>
+
+<script>
+export default {
+  props: {
+    linkedTo: { type: String, default: '/' }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
